f476439c6ad4ad592811f1e9c067fe5da72ed109
Nurdoğan Bahadır <nurdoganbahadirr@gmail.com>
2024-08-23 14:50:12 +0300

    sass dosyaları oluşturuldu

--- src/App.js
+++ src/App.js
@@ -1,11 +1,9 @@
-import './App.css';
+import "./App.css";
 
 function App() {
-  return (
-    <>
-    
-    </>
-  );
+  return <>
+  
+  </>;
 }
 
 export default App;
---
+++ src/components/ProductCard/ProductCard.jsx
@@ -0,0 +1,1 @@
+import "./ProductCard.scss";
