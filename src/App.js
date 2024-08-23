3cafa26f025abca301a9a302bd726ba4eaff01a0
Nurdoğan Bahadır <nurdoganbahadirr@gmail.com>
2024-08-23 15:28:18 +0300

    productCard'a geçildi html öeğleri oluşturuldu ve data.js'ten image'lar çekildi

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import "./App.css";
 import Header from "./components/Header/Header";
+import ProductCard from "./components/ProductCard/ProductCard";
 
 function App() {
   return (
     <>
       <Header />
+      <ProductCard />
     </>
   );
 }
--- src/components/ProductCard/ProductCard.jsx
+++ src/components/ProductCard/ProductCard.jsx
@@ -1,1 +1,21 @@
-import "./ProductCard.scss";
+import "./ProductCard.css";
+import { products } from "../../helper/data.js";
+
+import React from "react";
+
+function ProductCard() {
+  return (
+    <div className="container">
+      <div className="productCards">
+        {products.map((item) => (
+          <div className="card">
+            <img src={item.image} alt="" />
+            
+          </div>
+        ))}
+      </div>
+    </div>
+  );
+}
+
+export default ProductCard;
