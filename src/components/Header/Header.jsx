75e8bf84777ee8d441a887de6e09728c342bb5b7
Nurdoğan Bahadır <nurdoganbahadirr@gmail.com>
2024-08-23 14:55:40 +0300

    header html öğeleri oluşturuldu

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,1 +1,20 @@
-import "./Header.scss";
+import "./Header.css";
+
+import React from "react";
+
+function Header() {
+  return (
+    <div className="header">
+      <h1 className="title">Products List</h1>
+      <div className="menu">
+        <button>All</button>
+        <button>ELECTRONİCS</button>
+        <button>JEWELERY</button>
+        <button>MEN'S CLOTHİNS</button>
+        <button>WOMEN'S CLOTHİNGS</button>
+      </div>
+    </div>
+  );
+}
+
+export default Header;
